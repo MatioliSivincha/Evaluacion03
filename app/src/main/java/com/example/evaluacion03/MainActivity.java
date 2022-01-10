ca1cfc8c846c6e60de33db22935a3627b6632af5
unknown <MatioliSivincha>
2022-01-09 20:39:47 -0500

    diseño del setting

--- app/src/main/java/com/example/evaluacion03/MainActivity.java
+++ app/src/main/java/com/example/evaluacion03/MainActivity.java
@@ -1,18 +1,36 @@
 package com.example.evaluacion03;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity implements NavigationHost {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         if (savedInstanceState ==null){
             getSupportFragmentManager()
                     .beginTransaction()
@@ -20,7 +38,6 @@ public class MainActivity extends AppCompatActivity implements NavigationHost {
                     .commit();
         }
     }
-
     @Override
     public void navigateTo(Fragment fragment, boolean addToBackstack){
         FragmentTransaction transaction=
