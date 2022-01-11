13509f725790dc94ad7d97a282674771cd54ef54
unknown <MatioliSivincha>
2022-01-10 21:06:04 -0500

    Configuracion del boton Settings

--- app/src/main/java/com/example/evaluacion03/HomeFragment.java
+++ app/src/main/java/com/example/evaluacion03/HomeFragment.java
@@ -8,9 +8,27 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.button.MaterialButton;
+
 public class HomeFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
     @Override
     public View onCreateView (@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home_fragment, container, false);
+        View view=inflater.inflate(R.layout.home_fragment,container,false);
+        MaterialButton settingsButton=view.findViewById(R.id.setting_button);
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((NavigationHost) getActivity()).navigateTo(new SettingsFragment(),false);
+            }
+        });
+
+        return view;
     }
 }
